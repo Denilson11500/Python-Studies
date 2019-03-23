351c1552bbd1d739c98b607a6d7ac7620ee541a4
denilson <42578438+Denilson11500@users.noreply.github.com>
2019-03-23 18:52:42 -0300

    Update SortAlgorithms.py
    Update to show step by step if n <= 10.

--- SortAlgorithms/SortAlgorithms.py
+++ SortAlgorithms/SortAlgorithms.py
@@ -1,6 +1,9 @@
 import random
 from time import time
 
+print('Se tamanho menor ou igual a 10, será mostrado o passo a passo do processo.')
+n = int(input('Digite o tamanho do array a ser organizado: '))
+
 
 def bubble_sort(v):
     sorted = False
@@ -9,6 +12,8 @@ def bubble_sort(v):
         for i in range(0, len(v) - 1):
             if v[i] > v[i + 1]:
                 v[i], v[i + 1] = v[i + 1], v[i]
+                if n <= 10:
+                    print(v)
                 sorted = False
 
 
@@ -23,6 +28,8 @@ def quick_sort(v, start, end):
             j -= 1
         if i < j:
             v[i], v[j] = v[j], v[i]
+            if n <= 10:
+                print(v)
             i += 1
             j -= 1
         elif i == j:
@@ -61,6 +68,8 @@ def merge(v, start, half, end):
         v[k] = v2[j]
         j += 1
         k += 1
+    if n <=10:
+        print(f'{v1} + {v2} = {v[start:end+1]}')
 
 
 def merge_sort(v, start, end):
@@ -92,6 +101,8 @@ def shift_down(v, start, end):
             return
         else:
             v[root], v[swap] = v[swap], v[root]
+            if n <= 10:
+                print(v)
             root = swap
 
 
@@ -107,11 +118,11 @@ def heap_sort(v, length):
     heapify(v, length)
     while end > 0:
         v[0], v[end] = v[end], v[0]
+        print(v)
         end -= 1
         shift_down(v, 0, end)
 
 
-n = int(input('Digite o tamanho do array a ser organizado: '))
 array = []
 array2 = []
 array3 = []
@@ -123,23 +134,34 @@ for i in range(n):
     array3.append(array[i])
     array4.append(array[i])
 
-print(f'Vetor aleatório de tamanho {n} :\n{array}')
-print('Executando bubble sort, é lento para tamanhos muito grandes.')
+print(f'Vetor aleatório de tamanho {n} :\m')
+print(f'\nExecutando bubble sort, é lento para tamanhos muito grande:\n{array}')
+if n <= 10:
+    print('Exibido o vetor após cada troca.')
 t0 = time()
 bubble_sort(array)
 tf = time()
 print(f'Usando bubble sort:\n{array}')
 print(f'Tempo usando bubble sort: {tf-t0}')
+print(f'\nExecutando quick sort:\n{array2}')
+if n <= 10:
+    print('Exibido o vetor após cada troca, atenção pode ocorrer trocas de um elemento com ele mesmo.')
 t0 = time()
 quick_sort(array2, 0, len(array2)-1)
 tf = time()
 print(f'Usando quick sort:\n{array2}')
 print(f'Tempo usando quick sort: {tf-t0}')
+print(f'\nExecutando merge sort:\n{array3}')
+if n <= 10:
+    print('Exibido o vetor após cada merge, pode ser que nada seja alterado.')
 t0 = time()
 merge_sort(array3, 0, len(array3)-1)
 tf = time()
 print(f'Usando merge sort:\n{array3}')
 print(f'Tempo usando merge sort: {tf-t0}')
+print(f'\nExecutando heap sort:\n{array4}')
+if n <= 10:
+    print('Exibido o vetor após cada troca.')
 t0 = time()
 heap_sort(array4, len(array4))
 tf = time()
